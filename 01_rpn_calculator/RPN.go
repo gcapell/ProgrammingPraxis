1b61fc7569b063d4632125cca34c23d4602f83cb
Gary Capell <gary.capell@gmail.com>
2012-02-11 19:36:50 +1100

    gofmt

--- 01_rpn_calculator/RPN.go
+++ 01_rpn_calculator/RPN.go
@@ -3,8 +3,8 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"os"
 	"log"
+	"os"
 	"strconv"
 )
 
@@ -14,19 +14,23 @@ func main() {
 
 	for _, sym := range bytes.Fields(b.Bytes()) {
 
-			switch(sym[0]) {
-				case '+': fmt.Println("plus")
-				case '-': fmt.Println("minus")
-				case '*': fmt.Println("multiply")
-				case '/': fmt.Println("divide")
-				default: 
-					s := string(sym)
-					f, err := strconv.ParseFloat(s, 64)
-					if err != nil {
-						log.Panic(s, err)
-					}
-					fmt.Println(f)
+		switch sym[0] {
+		case '+':
+			fmt.Println("plus")
+		case '-':
+			fmt.Println("minus")
+		case '*':
+			fmt.Println("multiply")
+		case '/':
+			fmt.Println("divide")
+		default:
+			s := string(sym)
+			f, err := strconv.ParseFloat(s, 64)
+			if err != nil {
+				log.Panic(s, err)
 			}
+			fmt.Println(f)
+		}
 
 	}
 
