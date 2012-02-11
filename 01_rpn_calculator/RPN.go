365cf8794b4afc05e25df9e9888d036a76a9b297
Gary Capell <gary.capell@gmail.com>
2012-02-11 20:05:22 +1100

    Operators handled as a map, operators now applied to stack

--- 01_rpn_calculator/RPN.go
+++ 01_rpn_calculator/RPN.go
@@ -8,24 +8,32 @@ import (
 	"strconv"
 )
 
+type op_fn  func(float64,float64) float64
+
+var operators = map[byte] op_fn {
+	'+': func(a,b float64) float64 {return a+b},
+	'-': func(a,b float64) float64 {return a-b},
+	'*': func(a,b float64) float64 {return a*b},
+	'/': func(a,b float64) float64 {return a/b},
+}
+
 func main() {
 	var b bytes.Buffer
 	b.ReadFrom(os.Stdin)
+	log.SetFlags(0)	// quieter logging
 
 	stack := make([]float64, 0, 50)
 
 	for _, sym := range bytes.Fields(b.Bytes()) {
 
-		switch sym[0] {
-		case '+':
-			fmt.Println("plus")
-		case '-':
-			fmt.Println("minus")
-		case '*':
-			fmt.Println("multiply")
-		case '/':
-			fmt.Println("divide")
-		default:
+		op, ok := operators[sym[0]]
+		if ok {
+			tos := len(stack)
+			newValue := op(stack[tos-2], stack[tos-1])
+			log.Printf("%.2f %s %.2f = %.2f", stack[tos-2], string(sym[0]), stack[tos-1], newValue)
+			stack[tos-2] = newValue
+			stack = stack[:tos-1]
+		} else {
 			s := string(sym)
 			f, err := strconv.ParseFloat(s, 64)
 			if err != nil {
