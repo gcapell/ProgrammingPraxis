ae801eeaf167b641e8e82071dc938e70ee5a12fe
Gary Capell <gary.capell@gmail.com>
2012-02-12 18:26:05 +1100

    Loads board, prints board

--- 04_sudoku/sudoku.go
+++ 04_sudoku/sudoku.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"log"
+	"strconv"
 )
 
 const (
-	puzzle1 =   `
+	SIZE     = 9
+	NSQUARES = SIZE * SIZE
+	puzzle1  = `
 	4.. ... 8.5
 	.3. ... ...
 	... 7.. ...
@@ -20,9 +24,69 @@ const (
 	`
 )
 
+type (
+	Board [NSQUARES]uint8
+	Pos   struct{ row, col int }
+)
 
 func main() {
 	log.SetFlags(0) // quieter logging
 
-	log.Print(puzzle1)
+	var b Board
+
+	b.LoadFrom(puzzle1)
+	log.Print(&b)
+}
+
+func (p *Pos) Next() {
+	p.col += 1
+	if p.col == SIZE {
+		p.col = 0
+		p.row += 1
+	}
+}
+
+func (p *Pos) pos() int {
+	return p.row*SIZE + p.col
+}
+
+func (b *Board) Assign(p Pos, n uint8) {
+	b[p.pos()] = n
+}
+
+func cellString(c uint8) string {
+	if c == 0 {
+		return "."
+	}
+	return strconv.Itoa(int(c))
+
+}
+
+func (b *Board) String() string {
+
+	var buf bytes.Buffer
+	for row := 0; row < SIZE; row++ {
+		for col := 0; col < SIZE; col++ {
+			buf.WriteString(cellString(b[row*SIZE+col]))
+			if col%3 == 2 {
+				buf.WriteString(" ")
+			}
+		}
+		buf.WriteString("\n")
+	}
+	return buf.String()
+}
+
+func (b *Board) LoadFrom(s string) {
+	var p Pos
+	for _, c := range s {
+		if c == '.' {
+			p.Next()
+		}
+		if c >= '1' && c <= '9' {
+			n, _ := strconv.Atoi(string(c))
+			b.Assign(p, uint8(n))
+			p.Next()
+		}
+	}
 }
